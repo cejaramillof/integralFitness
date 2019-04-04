834f3b4d2f47feaaa7fa0d39e5225d31288e576d
Carlos Jaramillo <cejaramillof@gmail.com>
2019-04-04 02:15:51 +0000

    create users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+    MUSCLE_BASE_URL = "https://jhnutricion.com/"
+    # "https://evolutionfitimg.teamsystem.com/"
+    $base_url = MUSCLE_BASE_URL
 end
